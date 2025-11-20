e2c35a2dd47c67ef53985ebb9e6c112a31741877
chagoya27 <leocingon@gmail.com>
2025-11-19 18:44:48 -0600

    modifico archivo vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/css/estilos.css','resources/css/estilosFormulario','resources/css/estilosHome.css','resources/css/estilosNavBar.css'],
             refresh: true,
         }),
     ],
